0ea423cecff22592c2a997098299b1087c54f1a9
Hunter Metts <hunter.metts@owentechs.com>
2020-06-04 09:18:01 -0500

    emailTest

--- AjaxEmail.php
+++ AjaxEmail.php
@@ -24,11 +24,11 @@ if($_POST){
              "MIME-Version: 1.0\r\n" . 
              "X-Mailer: PHP/" . phpversion() . "\r\n";
  
-  /* PREVENT EMAIL INJECTION */
-  if ( preg_match("/[\r\n]/", $name) || preg_match("/[\r\n]/", $email) ) {
-    header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
-    die("500 Internal Server Error");
-  }
+  // /* PREVENT EMAIL INJECTION */
+  // if ( preg_match("/[\r\n]/", $name) || preg_match("/[\r\n]/", $email) ) {
+  //   header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+  //   die("500 Internal Server Error");
+  // }
 
   /* MESSAGE TEMPLATE */
   $mailBody = "Name: $name \n\r" .
